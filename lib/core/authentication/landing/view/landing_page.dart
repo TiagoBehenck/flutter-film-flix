07c6e13adaba090b2fd7e21357f07d2428fd11d0
Tiago Behenck <tiago.behenck.santos@gmail.com>
2025-01-14 17:18:22 -0300

    first commit

---
+++ lib/core/authentication/landing/view/landing_page.dart
@@ -0,0 +1,73 @@
+import 'package:filme_flix/core/authentication/login/view/login_page.dart';
+import 'package:filme_flix/widgets/buttons/primary_button.dart';
+import 'package:filme_flix/widgets/buttons/secondary_button.dart';
+import 'package:flutter/material.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Material(
+      child: Stack(
+        alignment: Alignment.bottomCenter,
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              "assets/images/the_last_jedi.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            height: size.height * .33,
+            width: size.width,
+            padding: const EdgeInsets.symmetric(vertical: 29, horizontal: 19),
+            decoration: BoxDecoration(
+              color: Colors.black.withOpacity(.9),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(50),
+                topRight: Radius.circular(50),
+              ),
+            ),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "What movies anytime anywhere",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+                const Text(
+                  "Explore a vast collection of blockbuster movies, timeless classics and the latest releases.",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 13,
+                  ),
+                ),
+                PrimaryButton(
+                  onPressed: () {
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      ),
+                    );
+                  }, 
+                  text: "Login",
+                ),
+                SecondaryButton(
+                  onPressed: () {},
+                  text: "Sign Up",
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
---
+++ lib/widgets/buttons/primary_button.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  final void Function()? onPressed;
+  final String text;
+  const PrimaryButton({
+    super.key,
+    required this.onPressed,
+    required this.text
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        width: size.width,
+        height: 52,
+        child: ElevatedButton(
+          onPressed: onPressed,
+          style: ElevatedButton.styleFrom(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(4)),
+            ),
+            backgroundColor: const Color(0xff32A873),
+          ),
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Color(0xff121212),
+              fontWeight: FontWeight.bold,
+              fontSize: 14,
+            ),
+          ),
+          ),
+      ),
+    );
+  }
+}
