ae380bf07c857775134d47e6943fefbe379b0808
Tiago Behenck <tiago.behenck.santos@gmail.com>
2025-04-23 15:36:54 -0300

    refact the widget

--- lib/widgets/banner_movie/banner_movie.dart
+++ lib/widgets/banner_movie/banner_movie.dart
@@ -1,28 +1,19 @@
-import 'package:filme_flix/common/extensions/build_context_extension.dart';
 import 'package:filme_flix/widgets/banner_movie/banner_movie_error.dart';
 import 'package:flutter/material.dart';
 
 class BannerMovie extends StatelessWidget {
   const BannerMovie({
     super.key,
-    required this.src,
   });
-  final String src;
-
+  
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: context.height * 0.6,
-      width: double.infinity,
-      child: Image.network(
-        src,
+    return  Image.asset(
+        "assets/images/banner.png",
         fit: BoxFit.cover,
-        loadingBuilder: (context, child, loadingProgress) {
-          if (loadingProgress == null) return child;
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+        alignment: Alignment.topCenter,
+        width: double.infinity,
+        height: double.infinity,
         errorBuilder: (context, error, stackTrace) {
           return BannerMovieError(
             onRetry: () {
@@ -32,7 +23,6 @@ class BannerMovie extends StatelessWidget {
             },
           );
         },
-      ),
     );
   }
 }
