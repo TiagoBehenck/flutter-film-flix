3055e3e9c61ca2e8262d57d05d9041a4e58b406f
Tiago Behenck <tiago.behenck.santos@gmail.com>
2025-04-23 15:36:40 -0300

    create structure to fetch movies from API

---
+++ lib/models/movie.dart
@@ -0,0 +1,19 @@
+class Movie {
+  final int id;
+  final String? title;
+  final String? imagePath;
+
+  Movie({
+    required this.id,
+    this.title,
+    this.imagePath,
+  });
+
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+      id: json['id'],
+      title: json['title'],
+      imagePath: json['poster_path'] ?? '',
+    );
+  }
+}
