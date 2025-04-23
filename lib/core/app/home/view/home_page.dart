583dbcb0056f71c56eaf06987743936d6c4a9b31
Tiago Behenck <tiago.behenck.santos@gmail.com>
2025-04-23 16:10:29 -0300

    improve code

--- lib/core/app/home/view/home_page.dart
+++ lib/core/app/home/view/home_page.dart
@@ -2,7 +2,6 @@ import 'package:filme_flix/common/extensions/build_context_extension.dart';
 import 'package:filme_flix/widgets/banner_movie/banner_movie.dart';
 import 'package:filme_flix/widgets/movie_carrossel/movie_carrossel.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -15,56 +14,20 @@ class HomePage extends StatelessWidget {
         physics: const BouncingScrollPhysics(),
         slivers: [
           SliverAppBar(
-            systemOverlayStyle: const SystemUiOverlayStyle(
-              statusBarColor: Colors.transparent,
-              statusBarIconBrightness: Brightness.light, 
-            ),
             backgroundColor: Colors.transparent,
             expandedHeight: context.height * 0.6,
             pinned: true,
-            floating: true,
-            snap: true, 
             elevation: 0,
-            toolbarHeight: 0,
+            floating: false,
+            stretch: true,
+            forceMaterialTransparency: true,
             collapsedHeight: kToolbarHeight,
-            flexibleSpace: FlexibleSpaceBar(
-              collapseMode: CollapseMode.pin,
-              background: Stack(
-                fit: StackFit.expand,
-                children: [
-                  // Imagem de fundo
-                  const Positioned.fill(
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        bottomLeft: Radius.circular(10),
-                        bottomRight: Radius.circular(10),
-                      ),
-                      child: BannerMovie(),
-                    ),
-                  ),
-                  // Opcional: Gradiente para melhorar a visibilidade do título
-                  Positioned.fill(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(10),
-                          bottomRight: Radius.circular(10),
-                        ),
-                        gradient: LinearGradient(
-                          begin: Alignment.topCenter,
-                          end: Alignment.bottomCenter,
-                          colors: [
-                     Colors.black.withOpacity(0.6), // Mais escuro no topo para a AppBar
-                            Colors.transparent,
-                            Colors.black.withOpacity(0.7),
-                          ],
-                          stops: const [0.0, 0.3, 1.0],
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+            flexibleSpace: const FlexibleSpaceBar(
+              collapseMode: CollapseMode.parallax,
+              stretchModes: [
+                StretchMode.zoomBackground,
+              ],
+              background: BannerMovie(),
             ),
           ),
           SliverToBoxAdapter(
@@ -78,7 +41,7 @@ class HomePage extends StatelessWidget {
               const MovieCarrossel(
                 categoryTitle: 'New Arrivals',
               ),
-              // Outros widgets aqui
+              const SizedBox(height: 500),
             ]),
           ),
         ],
--- lib/widgets/banner_movie/banner_movie.dart
+++ lib/widgets/banner_movie/banner_movie.dart
@@ -5,24 +5,44 @@ class BannerMovie extends StatelessWidget {
   const BannerMovie({
     super.key,
   });
-  
+
   @override
   Widget build(BuildContext context) {
-    return  Image.asset(
-        "assets/images/banner.png",
-        fit: BoxFit.cover,
-        alignment: Alignment.topCenter,
-        width: double.infinity,
-        height: double.infinity,
-        errorBuilder: (context, error, stackTrace) {
-          return BannerMovieError(
-            onRetry: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                const SnackBar(content: Text('Trying again...')),
-              );
-            },
-          );
-        },
+    return Stack(
+      children: [
+        Image.asset(
+          "assets/images/banner.png",
+          fit: BoxFit.cover,
+          alignment: Alignment.topCenter,
+          width: double.infinity,
+          height: double.infinity,
+          errorBuilder: (context, error, stackTrace) {
+            return BannerMovieError(
+              onRetry: () {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Trying again...')),
+                );
+              },
+            );
+          },
+        ),
+        Positioned.fill(
+          child: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.black.withOpacity(0.6),
+                  Colors.transparent,
+                  Colors.black.withOpacity(0.7),
+                ],
+                stops: const [0.0, 0.3, 1.0],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
