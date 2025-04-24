f775b0af119ee53d6ee44a264ccd2ddbd79ca169
Tiago Behenck <tiago.behenck.santos@gmail.com>
2025-04-24 11:05:26 -0300

    add padding

--- lib/widgets/search_input/search_input.dart
+++ lib/widgets/search_input/search_input.dart
@@ -12,28 +12,34 @@ class _SearchInputState extends State<SearchInput> {
   Widget build(BuildContext context) {
     return TextField(
       style: TextStyle(
-        color: Theme.of(context).colorScheme.primary,
+      color: Theme.of(context).colorScheme.primary,
       ),
       decoration: InputDecoration(
-        hintText: 'Search for a title...',
-        hintStyle: TextStyle(
-          color: Theme.of(context).colorScheme.primary,
+      hintText: 'Search for a title...',
+      hintStyle: TextStyle(
+        color: Theme.of(context).colorScheme.primary,
+      ),
+      prefixIcon: const Padding(
+        padding: EdgeInsets.only(left: 8),
+        child: Icon(Icons.menu),
+      ),
+      prefixIconColor: Theme.of(context).colorScheme.primary,
+      suffixIcon: const Padding(
+        padding: EdgeInsets.only(right: 8),
+        child: Icon(Icons.search),
+      ),
+      suffixIconColor: Theme.of(context).colorScheme.primary,
+      filled: true,
+      fillColor: const Color(0xFF2B2930),
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(28),
+        borderSide: BorderSide(
+        color: Theme.of(context).colorScheme.primary,
         ),
-        prefixIcon: const Icon(Icons.menu),
-        prefixIconColor: Theme.of(context).colorScheme.primary,
-        suffixIcon: const Icon(Icons.search),
-        suffixIconColor: Theme.of(context).colorScheme.primary,
-        filled: true,
-        fillColor: const Color(0xFF2B2930),
-        focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(28),
-          borderSide: BorderSide(
-            color: Theme.of(context).colorScheme.primary,
-          ),
-        ),
-        enabledBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(28),
-        ),
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(28),
+      ),
       ),
     );
   }
