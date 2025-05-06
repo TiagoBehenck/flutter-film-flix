e65f0cbff254bffe7331d1b3e4c46e30d27d6666
Tiago Behenck <tiago.behenck.santos@gmail.com>
2025-05-06 08:43:56 -0300

    create a class to extends the models with methods Json serializable

---
+++ lib/models/json_serializable.dart
@@ -0,0 +1,7 @@
+abstract class JsonSerializable<T> {
+  Map<String, dynamic> toJson();
+  
+  static T fromJson<T extends JsonSerializable>(Map<String, dynamic> json, T Function(Map<String, dynamic>) fromJson) {
+    return fromJson(json);
+  }
+}
